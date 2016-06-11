9bbde698bf0eb6c8c946a0ea7e6b87e7f21e0228
ZSaberLv0 <z@zsaber.com>
2016-06-11 12:29:15 +0800

    update build script

--- tools/common/timestamp_save.bat
+++ tools/common/timestamp_save.bat
@@ -24,7 +24,7 @@ echo %_CUR_TIME% > "%DIR_TO_CHECK%\%TIMESTAMP_FILE_NAME%"
 goto :EOF
 :GetUnixTime
 setlocal enableextensions
-for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do (
+for /f %%x in ('wmic path win32_utctime get /format:list ^<nul ^| findstr "="') do (
     set %%x)
 set /a z=(14-100%Month%%%100)/12, y=10000%Year%%%10000-z
 set /a ut=y*365+y/4-y/100+y/400+(153*(100%Month%%%100+12*z-3)+2)/5+Day-719469
