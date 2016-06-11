b3d871be070e64fd2bea748f61510340144a4906
ZSaberLv0 <z@zsaber.com>
2016-06-11 20:28:06 +0800

    update build script

--- tools/common/timestamp_save.bat
+++ tools/common/timestamp_save.bat
@@ -29,5 +29,6 @@ for /f %%x in ('wmic path win32_utctime get /format:list ^<nul ^| findstr "="') 
 set /a z=(14-100%Month%%%100)/12, y=10000%Year%%%10000-z
 set /a ut=y*365+y/4-y/100+y/400+(153*(100%Month%%%100+12*z-3)+2)/5+Day-719469
 set /a ut=ut*86400+100%Hour%%%100*3600+100%Minute%%%100*60+100%Second%%%100
+del /s/q TempWmicBatchFile.bat >nul 2>nul
 endlocal & set "%1=%ut%" & goto :EOF
 
